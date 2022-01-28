fa85af0bbe0b0a79149600e9d271c15ff5bdbe55
PhilosopherProgrammer <nekarakube@gmail.com>
2022-01-28 21:46:49 +0900

    creating card child element and add data

--- src/view.js
+++ src/view.js
@@ -1,4 +1,13 @@
 import jeremy from '../images/image-jeremy.png';
+import ellipsis from '../images/icon-ellipsis.svg';
+import work from "../images/icon-work.svg";
+import study from "../images/icon-study.svg";
+import social from "../images/icon-social.svg";
+import selfCare from "../images/icon-self-care.svg";
+import play from "../images/icon-play.svg";
+import exercise from "../images/icon-exercise.svg";
+
+const svgArr = [work, play, study, exercise, social, selfCare];
 
 export default (node, state) => {
     const template = document.querySelector("#time-tracking-container");
@@ -10,7 +19,7 @@ export default (node, state) => {
     return node.appendChild(clone);
 }
 
-function makeUser(node, state) {
+function makeUser(node) {
     const template = document.querySelector("#time-tracking-user");
     const clone = template.content.firstElementChild.cloneNode(true);
     clone.querySelector('img').setAttribute("src", jeremy);
@@ -21,12 +30,39 @@ function makeUser(node, state) {
 
 function makeCard(node, state) {
     const template = document.querySelector("#time-tracking-card");
-    const clone = template.content.firstElementChild.cloneNode(true)
-    state.data.forEach((item) => {
-        const el = document.createElement("h1")
-        el.innerText = item.title;
-        clone.appendChild(el);
-    });
+    state.data.forEach((item, idx) => {
+        const clone = template.content.firstElementChild.cloneNode(true)
+        
+        const markEl = document.createElement("img");
+        markEl.setAttribute("src", svgArr[idx]);
+        clone.querySelector(".mark").appendChild(markEl);
 
-    node.appendChild(clone);
+        const titleEl = document.createElement("h1")
+        titleEl.innerText = item.title;
+        clone.querySelector('.instruction__title').appendChild(titleEl);
+
+        const ellipsisEl = document.createElement("img");
+        ellipsisEl.setAttribute("src", ellipsis);
+        clone.querySelector('.instruction__ellipsis').appendChild(ellipsisEl);
+
+        const timeframeEl = document.createElement("div");
+        timeframeEl.innerText = `${item["timeframes"][state.currentFilter].current}hrs`;
+        clone.querySelector('.timeframe').appendChild(timeframeEl);
+        
+        const historyEl = document.createElement("div");
+        historyEl.innerText = `Last ${getPeriod(state.currentFilter)} - ${item["timeframes"][state.currentFilter].previous}hrs`;
+        clone.querySelector('.history').appendChild(historyEl);
+
+        node.appendChild(clone);
+    });
+}
+
+function getPeriod(currentFilter) {
+    if (currentFilter === "daily") {
+        return "Date";
+    } else if (currentFilter === "weekly") {
+        return "Week";
+    } else {
+        return "Month";
+    }
 }
